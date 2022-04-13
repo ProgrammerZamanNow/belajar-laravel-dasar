99940611cb524af13ff132d76e1956ad43ebc3b0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-13 14:08:38 +0700

    Testing

---
+++ tests/Unit/ContohTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ContohTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
